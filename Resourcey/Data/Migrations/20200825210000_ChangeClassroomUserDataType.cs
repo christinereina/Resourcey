41b3382b8e7fadf3cfa0c12468a6fb0a7980caff
Sara Kane <kane.sarak@gmail.com>
2020-08-25 16:59:46 -0700

    Add resources and sections views
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored-by: Teresa Rosinski <trosinski89@gmail.com>
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

---
+++ Resourcey/Data/Migrations/20200825210000_ChangeClassroomUserDataType.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Resourcey.Data.Migrations
+{
+    public partial class ChangeClassroomUserDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ClassroomCreator",
+                table: "Classrooms",
+                newName: "ClassroomCreatorId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassroomCreatorId",
+                table: "Classrooms",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classrooms_ClassroomCreatorId",
+                table: "Classrooms",
+                column: "ClassroomCreatorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classrooms_AspNetUsers_ClassroomCreatorId",
+                table: "Classrooms",
+                column: "ClassroomCreatorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classrooms_AspNetUsers_ClassroomCreatorId",
+                table: "Classrooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Classrooms_ClassroomCreatorId",
+                table: "Classrooms");
+
+            migrationBuilder.RenameColumn(
+                name: "ClassroomCreatorId",
+                table: "Classrooms",
+                newName: "ClassroomCreator");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassroomCreator",
+                table: "Classrooms",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
